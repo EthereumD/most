e8bf2a4a6ae42d4bb4b4bcfb51a17380e5ed7b88
PW-Chen <powei.chen.2016@ieee.org>
2016-10-19 14:23:51 +0800

    edit

---
+++ src/org/bitcoinj/core/test.java
@@ -0,0 +1,14 @@
+package org.bitcoinj.core;
+
+import org.bitcoinj.wallet.Wallet;
+
+public class test {
+
+	public static void main(String[] args) {
+		addWatchedAddress(kit.wallet().freshReceiveAddress());
+		System.out.println("New address created");
+
+
+	}
+
+}
